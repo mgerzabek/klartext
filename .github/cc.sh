6050e399382fcc2614c4d277815377c782d2ac6f
Michael Gerzabek <michael.gerzabek@gmail.com>
2021-10-11 21:50:37 +0200

    Mhm…

--- .github/cc.sh
+++ .github/cc.sh
@@ -1,9 +1,11 @@
 cd klartext
 MESSAGE="$(git log -1 --pretty=%B)"
 cd ../extended
-if [ -z "$(git status --porcelain)" ]; then exit 0 fi
-git config user.name github-actions
-git config user.email github-actions@github.com
-git add .
-git commit -m "klartext: $MESSAGE"
-git push
+if [ "$(git status --porcelain)" ]; 
+then
+  git config user.name github-actions
+  git config user.email github-actions@github.com
+  git add .
+  git commit -m "klartext: $MESSAGE"
+  git push
+fi
