eeed25d25b804b4dfd6f2fcdbf0e5fa963688f86
Michael Gerzabek <michael.gerzabek@gmail.com>
2021-10-12 06:37:30 +0200

    Skript passt noch immer nicht ganz…

--- .github/cc.sh
+++ .github/cc.sh
@@ -25,11 +25,11 @@ cp  ./_site/assets/style.css.map ./extended/assets/klartext.css.map
 cd klartext
 MESSAGE="$(git log -1 --pretty=%B)"
 cd ../extended
-if [ "$(git status --porcelain)" ]; 
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add .
+if [ -n "$(git status --porcelain)" ]; 
 then
-  git config user.name github-actions
-  git config user.email github-actions@github.com
-  git add .
   git commit -m "klartext: $MESSAGE"
   git push
 fi
