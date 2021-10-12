f8988590cc225ff68f6958b97454333fd895a180
Michael Gerzabek <michael.gerzabek@gmail.com>
2021-10-12 06:56:14 +0200

    Die -u wegnehmen bei cp

--- .github/cc.sh
+++ .github/cc.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 #
-cp -u ./klartext/_includes/button/cta.html ./extended/_includes/button/cta.html
-cp -u ./klartext/_includes/button/cta.w.alt.text.html ./extended/_includes/button/cta.w.alt.text.html
-cp -u ./klartext/_includes/icons/external-link.html ./extended/_includes/icons/external-link.html
-cp -u ./klartext/_includes/icons/link.html ./extended/_includes/icons/link.html
-cp -u ./klartext/_includes/testimonial/basic.html ./extended/_includes/testimonial/basic.html
-cp -u ./klartext/_includes/audio.html ./extended/_includes/audio.html
-cp -u ./klartext/_includes/credit.html ./extended/_includes/credit.html
-cp -u ./klartext/_includes/custom-head.html ./extended/_includes/custom-head.html
-cp -u ./klartext/_includes/custom-pre-closing-body.html ./extended/_includes/custom-pre-closing-body.html
-cp -u ./klartext/_includes/datum.html ./extended/_includes/datum.html
-cp -u ./klartext/_includes/figure.html ./extended/_includes/figure.html
-cp -u ./klartext/_includes/footer.html ./extended/_includes/footer.html
-cp -u ./klartext/_includes/header.html ./extended/_includes/header.html
-cp -u ./klartext/_includes/navigation.html ./extended/_includes/navigation.html
-cp -u ./klartext/_includes/sidebar.html ./extended/_includes/sidebar.html
-cp -u ./klartext/_includes/skin.html ./extended/_includes/skin.html
-cp -u ./klartext/_includes/social.html ./extended/_includes/social.html
-cp -u ./klartext/_includes/youtube.html ./extended/_includes/youtube.html
-cp -u ./klartext/_layouts/landing.html ./extended/_layouts/landing.html
-cp -u ./klartext/_layouts/page.html ./extended/_layouts/page.html
-cp -u ./klartext/_layouts/post.html ./extended/_layouts/post.html
-cp -u ./klartext/_layouts/video.html ./extended/_layouts/video.html
+cp ./klartext/_includes/button/cta.html ./extended/_includes/button/cta.html
+cp ./klartext/_includes/button/cta.w.alt.text.html ./extended/_includes/button/cta.w.alt.text.html
+cp ./klartext/_includes/icons/external-link.html ./extended/_includes/icons/external-link.html
+cp ./klartext/_includes/icons/link.html ./extended/_includes/icons/link.html
+cp ./klartext/_includes/testimonial/basic.html ./extended/_includes/testimonial/basic.html
+cp ./klartext/_includes/audio.html ./extended/_includes/audio.html
+cp ./klartext/_includes/credit.html ./extended/_includes/credit.html
+cp ./klartext/_includes/custom-head.html ./extended/_includes/custom-head.html
+cp ./klartext/_includes/custom-pre-closing-body.html ./extended/_includes/custom-pre-closing-body.html
+cp ./klartext/_includes/datum.html ./extended/_includes/datum.html
+cp ./klartext/_includes/figure.html ./extended/_includes/figure.html
+cp ./klartext/_includes/footer.html ./extended/_includes/footer.html
+cp ./klartext/_includes/header.html ./extended/_includes/header.html
+cp ./klartext/_includes/navigation.html ./extended/_includes/navigation.html
+cp ./klartext/_includes/sidebar.html ./extended/_includes/sidebar.html
+cp ./klartext/_includes/skin.html ./extended/_includes/skin.html
+cp ./klartext/_includes/social.html ./extended/_includes/social.html
+cp ./klartext/_includes/youtube.html ./extended/_includes/youtube.html
+cp ./klartext/_layouts/landing.html ./extended/_layouts/landing.html
+cp ./klartext/_layouts/page.html ./extended/_layouts/page.html
+cp ./klartext/_layouts/post.html ./extended/_layouts/post.html
+cp ./klartext/_layouts/video.html ./extended/_layouts/video.html
 cp  ./_site/assets/style.css ./extended/assets/klartext.css
 cp  ./_site/assets/style.css.map ./extended/assets/klartext.css.map
 cd klartext
